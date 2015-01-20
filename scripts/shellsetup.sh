29bedc646150f7eec12673dd32fe5ffccd7d85da
adam <adam@triazo.net>
2015-01-19 20:27:27 -0500

    Moves things around, adds shell script for automatic installation

---
+++ scripts/shellsetup.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Made January 2015 by triazo
+
+# The structure of the dotfiles dir mirrors that of the home folder.
+# Thus we can just list files that need to be linked
+emacsfiles=".emacs.d/lisp .emacs.d/bindings.el .emacs.d/modes .emacs.d/styles .emacs.d/init.el .emacs.d/tetris-scores .emacs.d/custom.el"
+basicfiles="${emacsfiles} .bash_aliases .tmux.conf .zshrc"
+xfiles="${basicfiles} scripts/p-urxvtc scripts/dwm_run.sh scripts/automon.sh config/dunst/dunstrc usr/src/dwm .xinitrc .xbindkeysrc .xmodmap"
+
+# Convinence function used below
+link () {
+    to="$1"
+    ! [ -d "$(dirname "$to")" ] && mkdir -p "$(dirname "$to")"
+    
+    if ! [ -h "$to" ]
+    then
+	[ -a "$to" ] && rm -r "$to"
+	ln -s "~/usr/dotfiles/$to" "$to"
+    fi
+}
+
+usage () {
+    cat <<EOF
+Usage: $0 [-xc]
+  -x  Include x configuration files
+  -c  Re-compile x utilities
+EOF
+}
+
+
+# get the options
+install_x=
+compile_x=
+while getopts ":xc" OPTION
+do
+    case $OPTION in
+	x)
+	    install_x="true"
+	    ;;
+	c)
+	    compile_x="true"
+	    ;;
+	?)
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
+# Have user install git
+if [[ -z $(which git) ]] 
+then
+  echo "Please install git before running this script"
+  exit 1
+fi
+
+
+
+# Clone the dotfiles directory if it is not already a git repo. otherwise, update
+mkdir -p usr/dotfiles
+cd ~/usr/dotfiles
+if [ -d .git ]
+then
+    git pull
+else
+    git clone https://github.com/triazo/dotfiles
+fi
+
+# Change back to the home directory
+cd ~
+
+if [ $install_x ]
+then
+    for file in ${xfiles}
+    do
+	echo "linking $file"
+	link $file
+    done
+else
+    for file in ${basicfiles}
+    do
+	echo "linking $file"
+	link $file
+    done
+fi
+
+
+echo "Install these packages to use everything:"
+echo "    gcc"
+echo "    dunst"

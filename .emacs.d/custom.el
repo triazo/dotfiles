154a2394781d6f59c062e4a8a0cc71b23ce81230
adam <adam@triazo.net>
2015-04-18 12:13:35 -0400

    Changes emacs dfaces and smooths workflow

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,7 +9,8 @@
  '(mpc-host "pi.triazo.net")
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "mail.triazo.net")
- '(smtpmail-smtp-service 587))
+ '(smtpmail-smtp-service 587)
+ '(vc-follow-symlinks t))
 
 (if (display-graphic-p)
     (progn (custom-set-variables
@@ -27,3 +28,11 @@
 
 
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "lime" :background "#ebebeb"))))
+ '(fringe ((t (:background "#ebebeb" :foreground "#6d8cc7"))))
+ '(scroll-bar ((t (:background "#ebebeb" :foreground "#6d8cc7" :height 1.0 :width condensed)))))

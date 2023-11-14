f806f27341f80093914066a8ea08d5bb5fdd4cd1
pager <pager@pager.codes>
2023-11-14 14:48:08 -0500

    Colors script start and adding a couple emacs files

--- scripts/shellsetup.sh
+++ scripts/shellsetup.sh
@@ -5,7 +5,7 @@
 # The structure of the dotfiles dir mirrors that of the home folder.
 # Thus we can just list files that need to be linked
 weechatfiles=".weechat/plugins.conf .weechat/aspell.conf .weechat/irc.conf .weechat/trigger.conf .weechat/sec.conf .weechat/exec.conf .weechat/weechat.conf .weechat/logger.conf .weechat/alias.conf .weechat/script.conf .weechat/xfer.conf .weechat/buffers.conf .weechat/relay.conf .weechat/charset.conf .weechat/python/anotify2.py scripts/weenotify.py scripts/relay.py"
-emacsfiles=".emacs.d/lisp .emacs.d/bindings.el .emacs.d/modes .emacs.d/styles .emacs.d/init.el .emacs.d/tetris-scores .emacs.d/custom.el .emacs.d/packages.el"
+emacsfiles=".emacs.d/lisp .emacs.d/bindings.el .emacs.d/modes .emacs.d/styles .emacs.d/init.el .emacs.d/tetris-scores .emacs.d/custom.el .emacs.d/packages.el .emacs.d/qinit.el .emacs.d/fairyfloss-theme.el"
 basicfiles="${emacsfiles} ${weechatfiles} .bash_aliases .tmux.conf .zshrc scripts/shellsetup.sh scripts/share.sh"
 xfiles="${basicfiles} scripts/p-urxvtc scripts/dwm_run.sh scripts/automon.sh scripts/xinit .config/dunst/dunstrc usr/src/dwm .xinitrc .xbindkeysrc .xmodmap scripts/touchpad.sh scripts/header.py scripts/screenie.sh"
 
